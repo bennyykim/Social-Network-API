d4625b4eeb84d187021dbdd911139f3967510c13
Benjamin Kim <bennykim523@gmail.com>
2022-02-28 16:37:28 -0500

    added routes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const { getUsers, 
+    getSingleUser, 
+    createUser, 
+    updateUser, 
+    deleteUser, 
+    addUser, 
+    removeUser 
+} = require('../../controllers/userController')
+
+router.route('/').get(getUsers).post(createUser);
+
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+
+router.route('/:userId/friends/:friendId').post(addUser).delete(removeUser);
+
+module.exports = router;
